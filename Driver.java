71e5c12ce73bdb4dc85485cb6c20b6c94413a4e5
Fergus <fergus.lole@outlook.com>
2023-04-06 22:01:13 +0100

    added simple menu system

--- Driver.java
+++ Driver.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.util.List;
 
 public class Driver {
@@ -5,11 +6,41 @@ public class Driver {
 
     public static void main(String[] args) {
         //load data
+        load();
 
         //display menu
+        int choice;
+        do {
+            choice = menu();
 
-        //run selection
+            //run selection
+        } while (choice != 4); //program loop
+    }
 
-        //on exit save data
+    private static void load(){
+
+    }
+
+    private static int menu(){
+        System.out.println("Menu");
+        System.out.println("=================");
+        System.out.println("1. Add a shift");
+        System.out.println("2. View Companies");
+        System.out.println("3. Generate Invoice");
+        System.out.println("4. Exit");
+        System.out.println("=================");
+        System.out.print("Enter an option: ");
+        int choice = -1;
+        boolean invalid = false;
+        do{
+            try {
+                choice = Integer.parseInt(System.console().readLine());
+            } catch (NumberFormatException e){
+                invalid = true;
+            }
+            if (invalid || choice < 1 || choice > 4) System.out.print("Enter an option between 1 and 4: ");
+        } while (choice > 4 || choice < 1);
+
+        return choice;
     }
 }
