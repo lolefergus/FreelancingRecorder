d9f4e9bd9bee17c8667d59eb606feed795a0f14c
Fergus <fergus.lole@outlook.com>
2023-04-06 21:18:43 +0100

    added driver class

---
+++ Driver.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Driver {
+    private List<Company> compaies;
+
+    public static void main(String[] args) {
+        //load data
+
+        //display menu
+
+        //run selection
+
+        //on exit save data
+    }
+}
