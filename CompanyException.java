f8d7a36c587dda2e89987a14067298905b9510d4
Fergus <fergus.lole@outlook.com>
2023-04-27 21:01:15 +0100

    added error class for company

--- CompanyException.java
+++ CompanyException.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CompanyExeption {
+public class CompanyException extends Exception {
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public CompanyException(String message) {
+        super(message);
+    }
 }
