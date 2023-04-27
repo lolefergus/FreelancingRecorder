7307344eec7c0b1a5903473402c07abd683cb7f8
Fergus <fergus.lole@outlook.com>
2023-04-27 21:00:58 +0100

    added display method

--- Company.java
+++ Company.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 /**
- * Represnts a company
+ * Represents a company
  */
 public class Company {
     private String name;
@@ -13,8 +13,13 @@ public class Company {
     /**
      * Creates basic company
      * @param name Company name
+     * @throws CompanyException if there's an invalid parameter
+     * @todo create proper name validation
      */
-    public Company(String name){
+    public Company(String name) throws CompanyException{
+        if(name.isEmpty()){
+            throw new CompanyException("Invalid Name");
+        }
         this.name = name;
     }
 
@@ -22,9 +27,14 @@ public class Company {
      * Creates full company - required for invoices
      * @param name Company name
      * @param email Company email
-     * @param address Address of company headquarters 
+     * @param address Address of company headquarters
+     * @throws CompanyException if there's an invalid parameter
+     * @todo create proper name, email and address validation
      */
-    public Company(String name, String email, String address){
+    public Company(String name, String email, String address) throws CompanyException{
+        if(name.isEmpty() || email.isEmpty() || address.isEmpty()){
+            throw new CompanyException("Invalid Details");
+        }
         this.name = name;
         this.email = email;
         this.address = address;
@@ -33,6 +43,7 @@ public class Company {
     /**
      * Getter method for company name
      * @return Company name
+     * @throws CompanyException if there's an invalid parameter
      */
     public String getName() {
         return name;
@@ -42,8 +53,8 @@ public class Company {
      * Getter method for company sessions
      * @return Array of sessions
      */
-    public Session[] getSessions() {
-        return (Session[]) sessions.toArray();
+    public List<Session> getSessions() {
+        return sessions;
     }
 
     @Override
@@ -71,5 +82,32 @@ public class Company {
      */
     public void addSession(SessionType type, String date, String description){
         sessions.add(new Session(type, date, description));
+    }
+
+    /**
+     * Prints company info to console
+     */
+    public void display() {
+        System.out.println(Driver.div+"\nCompany - "+name+"\n"+Driver.div);
+        if (email == null){
+            System.out.println("Invoicing not available");
+        }
+        else{
+            System.out.println("Invoicing available");
+            System.out.println("Email: " + email);
+            System.out.println("Address: "+ address);
+        }
+        if (sessionTypes == null){
+            System.out.println("0 registered session types");
+        }
+        else{
+            System.out.println(sessionTypes.size() + " registered session types");
+        }
+        if (sessions == null){
+            System.out.println("0 recorded sessions");
+        }
+        else{
+            System.out.println(sessions.size() + " recorded sessions");
+        }
     }
 }
